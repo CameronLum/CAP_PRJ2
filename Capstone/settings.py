5aca6d9e8f213abf9b64548ee08a94d0928100f0
CameronLum <37158179+CameronLum@users.noreply.github.com>
2018-03-08 11:54:29 -0500

    Update settings.py

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l8bw*d_k_i)+)uiseq(m44yn)48y@s@kcgp_x6n#^70_5s8_sg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
